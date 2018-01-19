0150870eb823ec19a070983599d3908086c5b36d
Archita21 <35433234+Archita21@users.noreply.github.com>
2018-01-20 01:51:06 +0530

    Add files via upload

---
+++ aaray.c
@@ -0,0 +1,30 @@
+ #include<stdio.h>
+   int main()
+   {
+    
+    int k;
+      sum=0;
+     
+    int i;
+     int a[10];
+     int n;
+     
+     
+     printf("Enter the array\n");
+     
+     
+     scanf("%d",&a[i]);
+     
+     for(i=0;i<=n;i++)
+     printf("enter the value\n");
+     scanf("%d",&k);
+   
+     
+     for(i=0;i<=k;i++)
+     {
+        sum=sum+a[i];
+        
+        }
+     printf("the sum is=%d",&sum);
+     return 0;
+     }
